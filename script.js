d99120f1d5838d89572a313c0dca40d2c4cf07d8
Mario <mario.tota.mt@gmail.com>
2020-11-19 16:46:07 +0100

    commit

---
+++ script.js
@@ -0,0 +1,33 @@
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// I numeri non possono essere duplicati.
+// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// L’utente non può inserire più volte lo stesso numero.
+// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
+// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
+// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+
+var array_bombe = [];
+var i = 1;
+while (i <= 16) {
+  randomNumber();
+  array_bombe.push(numeroGenerato);
+  checkDubleNumbers();
+  console.log(numeroGenerato);
+  i++;
+}
+console.log(array_bombe);
+
+//------------------------------------------------------------------------------
+function randomNumber(nMin, nMax) {
+  numeroGenerato = parseInt(Math.floor(Math.random() * 17) + 1);
+  return numeroGenerato;
+}
+
+function checkDubleNumbers() {
+  var result = false;
+  for (var i = 0; i < array_bombe.length; i++) {
+    if (numeroGenerato == array_bombe[i]) {
+      result = true;
+    }
+  }
+}
